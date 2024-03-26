a5c288fdad357f1e8dc8bd60d47fab5969547cb1
anton <anton.gap99@gmail.com>
2024-03-26 18:24:54 +0300

    feat: add some changes

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -21,7 +21,6 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions):webpack.WebpackPlug
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-
     ];
 
     isDev && plugins.push(new ReactRefreshWebpackPlugin());
