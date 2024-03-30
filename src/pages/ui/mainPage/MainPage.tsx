13d1561da3978f5b2b2b8bba4e9f69afd59684c2
Anton <anton.gap99@gmail.com>
2024-03-30 16:46:38 +0300

    update linter config

--- src/pages/ui/mainPage/MainPage.tsx
+++ src/pages/ui/mainPage/MainPage.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 function MainPage() {
