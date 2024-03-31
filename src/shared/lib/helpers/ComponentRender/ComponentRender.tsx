b71767edd1318cba55b6a6ce3703b85933807709
Anton <anton.gap99@gmail.com>
2024-03-31 17:40:28 +0300

    feat: add

--- src/shared/lib/helpers/ComponentRender/ComponentRender.tsx
+++ src/shared/lib/helpers/ComponentRender/ComponentRender.tsx
@@ -2,22 +2,28 @@ import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTest from 'shared/config/i18nConfig/i18nForTest';
-import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { StoreProvider } from 'app/providers/storeProvider';
+import { StateSchema } from 'app/providers/storeProvider/config/StateSchema';
 
 export interface ComponentRenderOptions {
-    route?: string
+    route?: string,
+	initialState?: StateSchema
 }
 
 export const ComponentRender = (component: ReactNode, options: ComponentRenderOptions = {}) => {
     const {
         route = '/',
+	    initialState,
     } = options;
 
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18nForTest}>
-                {component}
-            </I18nextProvider>
-        </MemoryRouter>,
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18nForTest}>
+                    {component}
+                </I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>,
     );
 };
