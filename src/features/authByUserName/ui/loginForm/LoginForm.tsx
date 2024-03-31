47331bee39c73d22ea7444e3c12627cd1a0c22fb
Anton <anton.gap99@gmail.com>
2024-03-31 21:18:57 +0300

    feat: add some changes

---
+++ src/features/authByUserName/ui/loginForm/LoginForm.tsx
@@ -0,0 +1,30 @@
+import cn from 'classnames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import { Input } from 'shared/ui/Input/Input';
+import style from './LoginForm.module.scss';
+
+interface LoginFormProps {
+    className?: string;
+
+}
+
+export const LoginForm = ({ className }: LoginFormProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={cn(style.LoginForm, className)}>
+            <Input
+                type="text"
+                className={style.input}
+                placeHolder={t('Логин')}
+                autoFocus
+            />
+            <Input
+                type="text"
+                className={style.input}
+                placeHolder={t('Пароль')}
+            />
+            <Button className={style.LoginBtn}>{t('Войти')}</Button>
+        </div>
+    );
+};
---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,59 @@
+import cn from 'classnames';
+import {
+    ChangeEvent, InputHTMLAttributes, memo, useEffect, useRef, useState,
+} from 'react';
+import { isDocumentVisible } from '@reduxjs/toolkit/dist/query/utils';
+import style from './Input.module.scss';
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+interface InputProps extends HTMLInputProps{
+    className?: string;
+	value?: string,
+	onChange?: (value: string) => void
+	type: 'text' | 'submit' | 'password' | 'range' | 'file'
+	placeHolder?: string,
+	autoFocus?: boolean
+}
+
+export const Input = memo((props: InputProps) => {
+    const {
+        className,
+        value,
+        onChange,
+        type = 'text',
+	    placeHolder,
+	    autoFocus = false,
+	    ...otherProps
+    } = props;
+
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e.target.value);
+    };
+
+    useEffect(() => {
+	    console.log('autoFocus', autoFocus);
+        if (autoFocus) {
+	        console.log('inputRef.current', inputRef.current);
+	        inputRef.current?.focus();
+        }
+    }, []);
+
+    return (
+        <div className={cn(style.InputWrapper, className)}>
+            {placeHolder && (
+                <div className={style.placeHolder}>
+                    {`${placeHolder}>`}
+                </div>
+            )}
+            <input
+                className={style.input}
+                type={type}
+                value={value}
+                onChange={onChangeHandler}
+                ref={inputRef}
+                {...otherProps}
+            />
+        </div>
+    );
+});
