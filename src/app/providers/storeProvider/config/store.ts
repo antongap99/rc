d8f2644f33fa241887da0a9733f534890830a4e9
Anton <anton.gap99@gmail.com>
2024-04-01 00:30:37 +0300

    feat: add some changes

--- src/app/providers/storeProvider/config/store.ts
+++ src/app/providers/storeProvider/config/store.ts
@@ -1,13 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/storeProvider/config/StateSchema';
 import { userReducer } from 'entities/user';
+import { loginReducer } from 'features/authByUserName/model/slice/LoginSlice';
 
 export function createReduxStore(initialState?: StateSchema) {
     return configureStore<StateSchema>({
         reducer: {
             user: userReducer,
+	        loginForm: loginReducer,
         },
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
 }
+
+export type RootState = ReturnType<typeof createReduxStore>['getState']
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = ReturnType<typeof createReduxStore>['dispatch']
